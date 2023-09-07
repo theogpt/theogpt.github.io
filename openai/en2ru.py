cddd54570966480006b52fea2e491d724f525ef5
theogpt <>
2023-09-07 16:38:19 -0700

    Updated science-of-sacraments

---
+++ openai/en2ru.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+# Avg price to translate a book: $100 per 1 million chars.
+import argparse
+import os
+import re
+import sys
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+if not openai.api_key:
+  sys.exit("OPENAI_API_KEY is missing. Obtain one at https://platform.openai.com/account/api-keys.")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', "--input", required=True, help='The input text file with non translated text')
+parser.add_argument('-o', "--output", required=True, help='The output text file with translated text')
+parser.add_argument('-c', "--context", required=True, help='The context file with translation instructions')
+parser.add_argument('-q', "--quality", type=int, default=1, help='The quality of translation')
+args = parser.parse_args()
+
+def is_translated(text):
+  if len(text) < 10:
+    return True
+  if re.match(r'^!\[\]|^#|^\<|^_|^\d+$|^\s*$', text):
+    return True
+  num_rus_chars = sum('а' <= char.lower() <= 'я' for char in text)
+  return num_rus_chars > 0
+
+def save_output(paragraphs):
+  print("Saving the output")
+  input_text = "\n\n".join(paragraphs)
+  with open(args.output, 'w') as f:
+      f.write(input_text)
+
+with open(args.context, 'r') as f:
+    gpt_context = f.read()
+
+# len(system + user + output) <= 8192
+# en char = 0.23 tokens
+# ru char = 0.47 tokens
+# $1 = 30K input tokens = 15K output tokens
+max_output_tokens = 4096
+max_output_chars = int(max_output_tokens / 0.50) # estimate
+max_input_tokens = int(8192 - max_output_tokens - len(gpt_context)*0.25)
+max_input_chars = int(max_input_tokens / 0.25) # estimate
+max_input_chars = min(4096, max_input_chars) # just in case
+
+max_translations = 500
+num_translations = 0
+
+def translate(text):
+  global num_translations
+  num_translations += 1
+  if num_translations > max_translations:
+    sys.exit('Reached the translations limit: ' + str(max_translations))
+
+  print('Waiting for a GPT response...')
+  response = openai.ChatCompletion.create(
+    model="gpt-4-0613",
+    messages=[
+      {
+        "role": "system",
+        "content": gpt_context
+      },
+      {
+        "role": "user",
+        "content": text
+      }
+    ],
+    temperature=1.00,
+    max_tokens=max_output_tokens, # output
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  return response.choices[0].message.content
+
+def translate2(text, iterations):
+  if iterations > 1:
+    print('Translating with', iterations, 'iterations')
+  resp = translate(text)
+  for i in range(1, iterations):
+    resp = translate(text + '\n+++++\n' + resp)
+  return resp
+
+if os.path.isfile(args.output):
+  if not input("Output file already exists. Overwrite? (y/n) ") in ["y", "Y"]:
+    exit(0)
+
+print("Analyzing the text...")
+
+with open(args.input, 'r') as f:
+    input_text = f.read()
+    paragraphs = re.split('\s*\n\s*\n\s*', input_text)
+
+print("  Total length: ", len(input_text))
+num_translated = sum(is_translated(para) for para in paragraphs)
+print("  Paragraphs count: ", len(paragraphs))
+print("  Not translated paragraphs: ", len(paragraphs) - num_translated)
+
+idx = 0
+while idx < len(paragraphs):
+  if is_translated(paragraphs[idx]):
+    idx += 1
+    continue
+
+  num = 1
+  sum_len = 0
+  while idx + num < len(paragraphs):
+    para = paragraphs[idx + num]
+    new_len = sum_len + len(para)
+    if is_translated(para) or new_len > max_input_chars:
+      break
+    sum_len = new_len
+    num += 1
+
+  text = "\n\n".join(paragraphs[idx:idx+num])
+  print(str(idx) + '..' + str(idx+num-1) + '/' + str(len(paragraphs)), 'en>')
+  print(text)
+
+  resp = translate2(text, args.quality)
+  print('ru>', resp)
+
+  for i in range(0, num):
+    paragraphs[idx + i] = ''
+  paragraphs[idx] = resp
+  save_output(paragraphs)
+  idx += num
---
+++ science-of-sacraments/gpt4.md
@@ -0,0 +1,6 @@
+You are a world-class translator of old books from English to Russian.
+1. Keep in mind that the books are about Catholic Sacraments written by a bishop in the 18th century. Thus, use the appropriate style in the translation. Avoid stylistic mistakes.
+2. A singular isolated x or X, that appears seemingly out of context, is often used to denote the holy cross. If that's the case, use the unicode symbol of the maltese cross (✠) in the output. Don't insert the maltese cross in other cases.
+3. Some of the words will appear corrupted. In such cases, guess the word from the context.
+4. Quotes or dialogues should be wrapped in underscores to make them visually distinct.
+5. When you are given both the original english text and a draft translation separated by 5 plus symbols (+++++), analyze both the original text and the draft, and write down a refined translation. Your output shall contain only the final version of your translation.
